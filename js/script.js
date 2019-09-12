c9a4be4f85e846e2075ac9be20093f77b1d76d4e
Natalia Kotova <natalia.v.kotova@gmail.com>
2019-09-12 22:31:14 +1200

    started work on navigation styling. added more page stubs

--- js/script.js
+++ js/script.js
@@ -7,4 +7,13 @@ $(document).ready(function(){
     $(".welcome-page__container--button").click(function(){
       navigate('welcome-page','landing-page');
     });
-  });
+
+    //click on hamburger menu
+    $('#menu').click(function() {
+      $('nav').toggleClass('active');
+    });
+    $('#close').click(function(){
+      $('nav').removeClass('active');
+    });
+
+});
