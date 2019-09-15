1e29acab104fb35ff4a1ee157140c55837dd5c1a
Natalia Kotova <natalia.v.kotova@gmail.com>
2019-09-15 20:49:43 +1200

    order page and other improvements

--- js/script.js
+++ js/script.js
@@ -3,9 +3,32 @@ function navigate(currentPage, nextPage){
     jQuery('#' + nextPage).show();
 }
 
+var items = [
+  {
+    id:1,
+    name:"VEGGIE BREKKIE (V, GF)",
+    price:18,
+  },
+  {
+    id:2,
+    name:"CRISPY CHICKEN BURGER & CAJUN FRIES",
+    price:20,
+  }
+]
+
+
 $(document).ready(function(){
     $(".welcome-page__container--button").click(function(){
       navigate('welcome-page','landing-page');
+    });
+
+    $("#brunch").click(function(){
+      navigate('landingPage','brunchPage');
+    });
+
+    $(".fa-shopping-cart").click(function(){
+      jQuery('#myOrder').show();
+      
     });
 
     //click on hamburger menu
