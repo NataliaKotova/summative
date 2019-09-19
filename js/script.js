d5b3c20cc4ea3705e6c1663236e11f010e458d66
Natalia Kotova <270028668@bun2-206-25.acg.ac.nz>
2019-09-19 15:18:51 +1200

    added javascript function source

--- js/script.js
+++ js/script.js
@@ -112,3 +112,22 @@ $('#orderButton').click(function(){
 
 
 });
+
+
+https://stackoverflow.com/questions/20618355/the-simplest-possible-javascript-countdown-timer
+
+window.onload = function() {
+  var hour = 2;
+  var sec = 60;
+  setInterval(function() {
+    document.getElementById("timer").innerHTML = hour + " : " + sec;
+    sec--;
+    if (sec == 00) {
+      hour--;
+      sec = 60;
+      if (hour == 0) {
+        hour = 2;
+      }
+    }
+  }, 1000);
+}
